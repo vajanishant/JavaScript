f0e1c5e3d538a146fa62a17f807fcad7d71ad298
Sagarmak <sagar1309@live.com>
2017-03-07 01:30:09 +0530

    Delete ex2.js

--- ex2.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    var h1 = document.getElementById('h1');
-    var par = h1.parentElement;
-    var h6 = document.createElement("h6");
-    var node = document.createTextNode(h1.innerHTML);
-    h6.appendChild(node);
-    par.replaceChild(h6, h1);
-}
